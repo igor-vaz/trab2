b8683c1406b6603866e0fad672ad6231df6a297f
Josué Pereira <jospdc@gmail.com>
2016-02-14 21:42:56 -0200

    Implementado metodo que calcula o proximo destino baseado na proximidade e na quantidade de pessoas

--- SCE.java
+++ SCE.java
@@ -7,6 +7,29 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+class Requisicao {
+	private static int id_gen;
+	private int id;
+	private int destino;
+
+	public Requisicao(int destino) {
+		this.id = id_gen++;
+		this.destino = destino;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public int getDestino() {
+		return this.destino;
+	}
+
+	public String toString() {
+	    return "(Requisicao ID: " + id + " Destino: " + destino + ")";
+	}
+}
+
 class Andar {
 	private int numero;
 	private ArrayList<Requisicao> fila;
@@ -32,6 +55,10 @@ class Andar {
 		this.fila.add(requisicao);
 	}
 
+	public int qtdRequisicoesFila() {
+		return this.fila.size();
+	}
+
 	public String toString() {
 	    return "(Andar Numero: " + numero + " fila: " + fila + ")";
 	}
@@ -40,16 +67,21 @@ class Andar {
 class Elevador extends Thread {
 	private Monitor monitor;
 	private static int id_gen;
-	private int id;
+	private int numero;
 	private int capacidade;
 	private Andar andarAtual;
 	private int qtdPessoas;
 
-	public Elevador(int capacidade, Andar andarAtual) {
-		this.id = id_gen++;
+	public Elevador(int capacidade, Andar andarAtual, Monitor monitor) {
+		this.numero = id_gen++;
 		this.capacidade = capacidade;
 		this.andarAtual = andarAtual;
 		this.qtdPessoas = 0;
+		this.monitor = monitor;
+	}
+
+	public int getNumero() {
+		return this.numero;
 	}
 
 	public int getCapacidade() {
@@ -65,39 +97,22 @@ class Elevador extends Thread {
 	}
 
 	public String toString() {
-	    return "(Elevador ID: " + id + " capacidade: " + capacidade + " andarAtual: " + andarAtual + " # de Pessoas: " + qtdPessoas + ")";
+	    return "(Elevador ID: " + numero + " capacidade: " + capacidade + " andarAtual: " + andarAtual + " # de Pessoas: " + qtdPessoas + ")";
 	}
 
 	//metodo executado pelas threads
-	public void run(){
+	public void run() {
+		/*
 		try{
 			for(int i = 0;i < monitor.getAndares().size(); i++){
 				monitor.irDestino(andarAtual.getNumero());
 			}
 		}catch(Exception e){}
-	}
-}
+		*/
 
-class Requisicao {
-	private static int id_gen;
-	private int id;
-	private int destino;
+		monitor.irDestino(this);
 
-	public Requisicao(int destino) {
-		this.id = id_gen++;
-		this.destino = destino;
-	}
-
-	public int getId() {
-		return this.id;
-	}
-
-	public int getDestino() {
-		return this.destino;
-	}
-
-	public String toString() {
-	    return "(Requisicao ID: " + id + " Destino: " + destino + ")";
+		
 	}
 }
 
@@ -108,11 +123,60 @@ class Monitor{
 		this.andares = _andares;
 	}
 
+	/* Não acho que isso deva existir...
 	public ArrayList<Andar> getAndares(){
 		return andares;
 	}
+	*/
 
-	public synchronized void irDestino(int andarAtual){
+
+	/**
+	 *	Calcula o próximo destino baseado na proximidade
+	 *	e na quantidade de requisições
+	 */
+	public Andar proxDestino(Elevador elevador){
+		int numeroAndarAtual = elevador.getAndarAtual().getNumero();
+
+		/* testes...
+		System.out.println("Elevador " + elevador.getNumero() + " andar atual: " + numeroAndarAtual);
+
+		for (int numero = 0 ; numero < andares.size() ; numero++)
+			System.out.println(numero + " " + andares.get(numero).qtdRequisicoesFila());
+		*/
+
+	    int min = andares.size();
+	    int closest = numeroAndarAtual;
+
+	    for (int i = 0 ; i < andares.size() ; i++) {
+	    	if(andares.get(i).qtdRequisicoesFila() != 0) {
+		        final int diff = Math.abs(i - numeroAndarAtual);
+
+		        if (diff < min) {
+		            min = diff;
+		            closest = i;
+		        }
+		    }
+	    }
+
+	    int closestRange = Math.abs(numeroAndarAtual - closest);
+
+		int numeroProxAndar = numeroAndarAtual - closestRange;
+		int qtdResquisicoes = andares.get(numeroAndarAtual - closestRange).qtdRequisicoesFila();
+
+	    if(andares.get(numeroAndarAtual + closestRange).qtdRequisicoesFila() > qtdResquisicoes) {
+	    	numeroProxAndar = numeroAndarAtual + closestRange;
+	    	qtdResquisicoes = andares.get(numeroAndarAtual + closestRange).qtdRequisicoesFila();
+	    }
+	    
+	    return andares.get(numeroProxAndar);
+	}
+
+	public synchronized void irDestino(Elevador elevador){
+
+		Andar proxAndar = this.proxDestino(elevador);
+
+		System.out.println("Elevador " + elevador.getNumero() + " proximo andar: " + proxAndar.getNumero());
+		
 
 	}
 
@@ -120,14 +184,14 @@ class Monitor{
 
 public class SCE {
 	public static void main(String[] args) {
-	Monitor monitor;
-	int qtdAndares = 0;
-	int qtdElevadores = 0;
-	int capacidade = 0;
-	ArrayList<Andar> andares = new ArrayList<Andar>();
-	ArrayList<Elevador> elevadores = new ArrayList<Elevador>();
-		
-	String[] splitted;
+		int qtdAndares = 0;
+		int qtdElevadores = 0;
+		int capacidade = 0;
+		ArrayList<Andar> andares = new ArrayList<Andar>();
+		ArrayList<Elevador> elevadores = new ArrayList<Elevador>();
+		Monitor monitor = new Monitor(andares);
+			
+		String[] splitted;
 
 		try{
 			// Open the file
@@ -139,7 +203,7 @@ public class SCE {
 			//Read File Line By Line
 
 			for(int linha = 0 ; (strLine = br.readLine()) != null ; linha++) {
-			// split the line on your splitter(s)
+				// split the line on your splitter(s)
 				splitted = strLine.split(" ");
 
 				switch(linha) {
@@ -156,7 +220,7 @@ public class SCE {
 					case 1:
 						for (int i = 0 ; i < qtdElevadores ; i++)
 							//cria threads
-							elevadores.add(new Elevador(capacidade, andares.get(Integer.parseInt(splitted[i]))));
+							elevadores.add(new Elevador(capacidade, andares.get(Integer.parseInt(splitted[i])), monitor));
 
 						break;
 
@@ -165,7 +229,7 @@ public class SCE {
 
 						for(int i = 0 ; i < qtd_pessoas ; i++)
 							andares.get(linha-2).addRequisicaoFila(new Requisicao(Integer.parseInt(splitted[i+1])));
-							
+								
 				}
 			}
 			//Close the input stream
@@ -174,11 +238,25 @@ public class SCE {
 		  	System.err.println("Error: " + e.getMessage());
 		}
 
+		/*
 		for (int j = 0 ; j < qtdAndares ; j++){
 			System.out.println(andares.get(j));
 		}
+		*/
+
+		//inicia as threads elevadores
 		for (int i = 0; i < qtdElevadores; i++) {
-			elevadores.get(i).run();
+			elevadores.get(i).start();
+		}
+
+
+		//espera pelo termino de todas as threads elevadores
+		for (int i = 0; i < qtdElevadores; i++) {
+			try {
+				elevadores.get(i).join();
+			} catch (InterruptedException e) {
+		  		System.err.println("Error: " + e.getMessage());
+			}
 		}
 
 	}
